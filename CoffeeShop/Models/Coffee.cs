8f89df375acb4dbc204fd03d0ae8da8102498328
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-01 11:11:44 -0700

    first commit

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Input is long")]
+        public string BeanType { get; set; }
+    }
+}
